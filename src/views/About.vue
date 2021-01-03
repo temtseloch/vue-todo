6a05cc8549505d97bec3110d7847d52ec7bfefef
Temtsel-Och Ulziisaikhan <temkalways@gmail.com>
2021-01-03 11:14:22 -0500

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList app v1.0.0. It is built using Vue.js and
+      jsonplaceholder as a mock api.
+    </p>
+  </div>
+</template>
